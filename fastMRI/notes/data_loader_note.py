90412868e636634c9402360edfc629ab1bf254ee
khulaifi95 <just4xzd1995@gmail.com>
2019-12-11 02:44:14 +0000

    Train02.

---
+++ fastMRI/notes/data_loader_note.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import h5py, os
+from functions import transforms as T
+from functions.subsample import MaskFunc
+from scipy.io import loadmat
+from torch.utils.data import DataLoader
+import numpy as np
+import torch
+from matplotlib import pyplot as plt
+
+
+
+def show_slices(data, slice_nums, cmap=None): # visualisation
+    fig = plt.figure(figsize=(15,10))
+    for i, num in enumerate(slice_nums):
+        plt.subplot(1, len(slice_nums), i + 1)
+        plt.imshow(data[num], cmap=cmap)
+        plt.axis('off')
+
+
+
+class MRIDataset(DataLoader):
+    def __init__(self, data_list, acceleration, center_fraction, use_seed):
+        self.data_list = data_list
+        self.acceleration = acceleration
+        self.center_fraction = center_fraction
+        self.use_seed = use_seed
+        
+    def __len__(self):
+        return len(self.data_list)
+    
+    def __getitem__(self, idx):
+        subject_id = self.data_list[idx]
+        return get_epoch_batch(subject_id, self.acceleration, self.center_fraction, self.use_seed)
+
+
+
+def get_epoch_batch(subject_id, acc, center_fract, use_seed=True):
+    ''' randomly select a few slices (batch_size) from each volume'''
+    
+    fname, rawdata_name, slice = subject_id
+    
+    with h5py.File(rawdata_name, 'r') as data:
+        rawdata = data['kspace'][slice]
+    
+    slice_kspace = T.to_tensor(rawdata).unsqueeze(0)
+    S, Ny, Nx, ps = slice_kspace.shape
+    
+    # apply random mask
+    shape = np.array(slice_kspace.shape)
+    mask_func = MaskFunc(center_fractions=[center_fract], accelerations=[acc])
+    seed = None if not use_seed else tuple(map(ord, fname))
+    mask = mask_func(shape, seed)
+    
+    # undersample
+    masked_kspace = torch.where(mask == 0, torch.Tensor([0]), slice_kspace)
+    masks = mask.repeat(S, Ny, 1, ps)
+    
+    # normalise data
+    img_gt, img_und = T.ifft2(slice_kspace), T.ifft2(masked_kspace)
+    norm = T.complex_abs(img_und).max()
+    if norm < 1e-6: norm = 1e-6
+    
+    img_gt, img_und, rawdata_und = img_gt/norm, img_und/norm, masked_kspace/norm
+    
+    return img_gt.squeeze(0), img_und.squeeze(0), rawdata_und.squeeze(0), masks.squeeze(0), norm
+
+
+
+def load_data_path(train_data_path, val_data_path):
+    ''' go through each subset (training, validation) and list all
+    the file names, the file paths and the slices of subjects in
+    the training and validation sets'''
+    
+    data_list = {}
+    train_and_val = ['train', 'val']
+    data_path = [train_data_path, val_data_path]
+    
+    for i in range(len(data_path)):
+        data_list[train_and_val[i]] = []
+        which_data_path = data_path[i]
+        for fname in sorted(os.listdir(which_data_path)):
+            subject_data_path = os.path.join(which_data_path, fname)
+            if not os.path.isfile(subject_data_path):
+                continue
+            
+            with h5py.File(subject_data_path, 'r') as data:
+                num_slice = data['kspace'].shape[0]
+            
+            # exclude the first 5 slices as they are noise
+            data_list[train_and_val[i]] += [(fname, subject_data_path, slice) for slice in range(5, num_slice)]
+    
+    return data_list
+
+
+
+if __name__ == '__main__': # only execute when as primary or called
+    
+    # load all file names, paths and slices
+    data_path_train = 'training sets'
+    data_path_val = 'validation set'
+    data_list = load_data_path(data_path_train, data_path_val)
+    
+    acc = 8
+    cen_fract = 0.04
+    seed = False # random masks for each slice
+    num_workers = 12 # data loading workers
+    
+    # create data loader for training set/ validation set
+    train_dataset = MRIDataset(data_list['train'], acceleration=acc, center_fraction=cen_fract, use_seed=seed)
+    train_loader = DataLoader(train_dataset, shuffle=True, batch_size=1, num_workers=num_workers)
+    for iteration, sample in enumerate(train_loader):
+        
+        img_gt, img_und, rawdata_und, masks, norm = sample
+        
+        # stack different slices into a volume for visualisation
+        A = masks[...,0].squeeze()
+        B = torch.log(T.complex_abs(rawdata_und) + 1e-9).squeeze()
+        C = T.complex_abs(img_und).squeeze()
+        D = T.complex_abs(img_gt).squeeze()
+        all_imgs = torch.stack([A, B, C, D], dim=0)
+        
+        # mask, masked space, undersampled image, ground truth
+        show_slices(all_imgs, [0, 1, 2, 3], cmap='gray')
+        plt.pause(1)
+        
+        if iteration >= 3: break # to show 4 random slices
+
---
+++ fastMRI/notes/functions_note.py
@@ -0,0 +1,186 @@
+import numpy as np
+import torch
+
+
+##
+###############################subsample###################################
+##
+class MaskFunc:
+    def __init__(self, center_fractions, accelerations):
+
+        if len(center_fractions) != len(accelerations):
+            raise ValueError('Number of center fractions should match number of accelerations')
+
+        self.center_fractions = center_fractions
+        self.accelerations = accelerations
+        self.rng = np.random.RandomState()
+
+    def __call__(self, shape, seed=None):
+
+        if len(shape) < 3:
+            raise ValueError('Shape should have 3 or more dimensions')
+
+        self.rng.seed(seed)     # ensure the same mask is generated
+        num_cols = shape[-2]    # number of columns of the mask; using -2 to double-check the shape
+
+        # Randomly make a choice from inputs
+        choice = self.rng.randint(0, len(self.accelerations))
+        center_fraction = self.center_fractions[choice]
+        acceleration = self.accelerations[choice]
+
+        # Create the mask
+        num_low_freqs = int(round(num_cols * center_fraction))  # number of columns of center fractions that kept in the mask
+        prob = (num_cols / acceleration - num_low_freqs) / (num_cols - num_low_freqs)   # percentage of columns to be masked
+        mask = self.rng.uniform(size=num_cols) < prob   # return a boolean distribution
+        pad = (num_cols - num_low_freqs + 1) // 2   # padding needed on two sides
+        mask[pad:pad + num_low_freqs] = True    # don't mask out the center fraction
+        # mask[pad:pad + num_low_freqs - 1] = True
+
+        # Solution to uncertainty of k-fold under-sampling strategy
+        # num_low_freqs = int(round(num_cols * center_fraction))
+        # prob = (num_cols / acceleration - num_low_freqs) / (num_cols - num_low_freqs)
+        # pad = (num_cols - num_low_freqs + 1) // 2
+        # mask_high = self.rng.uniform(size=num_cols - num_low_freqs) < prob
+        # mask_low = np.full(num_low_freqs, True)
+        # mask = np.concatenate([mask_high[:pad], mask_low, mask_high[pad + num_low_freqs:]])
+        
+        # Reshape the mask
+        mask_shape = [1 for _ in shape] # return a array of 1 as input shape
+        mask_shape[-2] = num_cols   # ensure mask match the original length 
+        mask = torch.from_numpy(mask.reshape(*mask_shape).astype(np.float32))
+
+        return mask
+
+
+##
+###############################transforms###################################
+##
+
+# 1)
+def tensor_to_complex_np(data):
+
+    data = data.numpy()
+    return data[..., 0] + 1j * data[..., 1]
+
+
+# 2)
+def to_tensor(data):
+    to_tensor(data):
+
+    if np.iscomplexobj(data):
+        data = np.stack((data.real, data.imag), axis=-1)
+    return torch..from_numpy(data)
+
+
+# 3)
+def apply_mask(data, mask_func, seed=None):
+
+    shape = np.array(data.shape)
+    shape[:-3] = 1
+    mask = mask_func(shape, seed)
+    return torch.where(mask == 0, torch.Tensor([0]), data), mask
+
+
+# 4)
+def fft2(data):
+
+    assert data.size(-1) == 2
+    data = ifftshift(data, dim=(-3, -2))
+    data = forch.fft(data, 2, normalized=True)
+    data = fftshift(data, dim=(-3, -2))
+    return data
+
+
+# 5)
+def ifft2(data):
+
+    assert data.size(-1) == 2
+    data = ifftshift(data, dim=(-3, -2))
+    data = torch.ifft(data, 2, normalized=True)
+    data = fftshift(data, dim=(-3, -2))
+    return data
+
+
+# 6)
+def complex_abs(data):
+    assert data.size(-1) == 2
+    return (data ** 2).sum(dim=-1).sqrt()
+
+
+# 7)
+def center_crop(data, shape):
+
+    assert 0 < shape[0] <= data.shape[-2]
+    assert 0 < shape[1] <= data.shape[-1]
+    w_from = (data.shape[-2] - shape[0]) // 2
+    h_from = (data.shape[-1] - shape[1]) // 2
+    w_to = w_from + shape[0]
+    h_to = h_from + shape[1]
+    return data[..., w_from:w_to, h_from:h_to, :]
+
+
+# 8)
+def complex_center_crop(data, shape):
+
+    assert 0 < shape[0] <= data.shape[-3]
+    assert 0 < shape[1] <= data.shape[-2]
+    w_from = (data.shape[-3] - shape[0]) // 2
+    h_from = (data.shape[-2] - shape[1]) // 2
+    w_to = w_from + shape[0]
+    h_to = h_from + shape[1]
+    return data[..., w_from:w_to, h_from:h_to, :]
+
+
+# 9)
+def normalize(data, mean, stddev, eps=0.):
+
+    return (data - mean) / (stddev + eps)
+
+
+# 10)
+def normalize_instance(data, eps=0.):
+
+    mean = data.mean()
+    std = data.std()
+    return normailize(data, mean, std, eps), mean, std
+
+
+# Helper functions
+
+def roll(x, shift, dim):
+
+    if isinstance(shift, (tuple, list)):
+        assert len(shift) == len(dim)
+        for s, d in zip(shift, dim):
+            x = roll(x, s, d)
+        return x
+    shift = shift % x.size(dim)
+    if shift == 0:
+        return x
+    left = x.narrow(dim, 0, x.size(dim) - shift)
+    right = x.narrow(dim, x.size(dim) - shift, shift)
+    return torch.cat((right, left), dim=dim)
+
+
+def fftshift(x, dim=None):
+
+    if dim is None:
+        dim = tuple(range(x.dim()))
+        shift = [dim // 2 for dim in x.shape]
+    elif isinstance(dim, int):
+        shift = x.shape[dim] // 2
+    else:
+        shift = [x.shape[i] // 2 for i in dim]
+    return roll(x, shift, dim)
+
+
+def ifftshift(x, dim=None):
+
+    if dim is None:
+        dim = tuple(range(x.dim()))
+        shift = [(dim + 1) // 2 for dim in x.shape]
+    elif isinstance(dim, int):
+        shift = (x.shape[dim] + 1) // 2
+    else:
+        shift = [(x.shape[i] + 1) // 2 for i in dim]
+    return roll(x, shift, dim)
